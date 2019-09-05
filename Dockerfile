f040f5ac0d4aaae51a4479950ba46a3cf0ee965b
Marko Mikulicic <mkm@bitnami.com>
2019-09-05 18:09:11 +0200

    Add getsumweb

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM golang:1.13 as builder
+
+WORKDIR /src
+
+COPY . .
+
+RUN go build ./cmd/getsumweb
+
+# Ideally we could use the "static" flavour but let's first start with the base flavour (which has glibc).
+FROM gcr.io/distroless/base@sha256:edc3643ddf96d75032a55e240900b68b335186f1e5fea0a95af3b4cc96020b77
+MAINTAINER Marko Mikulicic <mmikulicic@gmail.com>
+COPY --from=builder /src/getsumweb /usr/local/bin/
+
+EXPOSE 8080
+ENTRYPOINT ["getsumweb"]
---
+++ cmd/getsumweb/getsumweb.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var (
+	mainTemplate = template.Must(template.New("foo").Parse(`<html lang="en">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  {{.Head}}
+  <title>Immutable Artifact Checksum Database</title>
+  <style>
+    .container {
+      font-size: 1.6em;
+      line-height: 1.4;
+      margin: 3.5em auto;
+      max-width: 45em;
+      padding: 0 1.5em;
+    }
+    body {
+      font: 62.5% Arial, sans-serif;
+    }
+    code {
+      background-color: #f0f8ff;
+    }
+  </style>
+
+  <div class="container">
+  <h1>Immutable Artifact Checksum Database</h1>
+  {{.Body}}
+  </div>
+</html>
+`))
+)
+
+type mainTemplateData struct {
+	Head template.HTML
+	Body template.HTML
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var meta string
+	if len(r.URL.Path) > 1 {
+		scheme := "https"
+		if strings.HasPrefix(r.Host, "localhost:") {
+			scheme = "http"
+		}
+		meta = fmt.Sprintf(`<meta name="go-import" content="%s mod %s://%s">`, r.URL.Path[1:], scheme, r.Host)
+	}
+
+	body := fmt.Sprintf(`
+	<p>TODO</p>
+	<pre>%s</pre>`, template.HTMLEscapeString(meta))
+
+	data := &mainTemplateData{
+		Head: template.HTML(meta),
+		Body: template.HTML(body),
+	}
+	err := mainTemplate.Execute(w, data)
+	if err != nil {
+		reportError(w, r, err)
+		return
+	}
+}
+
+func reportError(w http.ResponseWriter, r *http.Request, err error) {
+	if os.IsNotExist(err) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	http.Error(w, err.Error(), http.StatusInternalServerError)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("Listening on port %s\n", port)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
